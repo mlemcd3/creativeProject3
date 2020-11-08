93b95ad4ee54b63a31778582069dbae21aa288a5
Emily McDermott <mlemcd3@gmail.com>
2020-11-07 20:43:26 -0800

    two player working

--- creative3/src/App.vue
+++ creative3/src/App.vue
@@ -1,20 +1,27 @@
-<template>
+<!-- <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-    <tickTackToe/>
+    <ticTacToe/>
+  </div>
+</template> -->
+
+
+<template>
+  <div id="app">
+    <tic-tac-toe />
   </div>
 </template>
 
 <script>
-import tickTackToe from "./components/Game.vue";
+import ticTacToe from "./components/Game.vue";
 
 export default{
   components:{
-    tickTackToe
+    ticTacToe
   },
 }
 </script>
--- creative3/src/components/Game.vue
+++ creative3/src/components/Game.vue
@@ -1,57 +1,87 @@
 <template>
-  <div class="grid" v-bind:board="board">
-    <div class="cell" v-for="cell in board" v-bind:key="cell" @click="cell.move = playerToken">
-        <button> {{cell.move}} </button>
-    </div>
+  <div class="tictactoe-board">
+      <div v-for="(n, i) in 3" v-bind:key="n">
+        <div v-for="(n, j) in 3" v-bind:key="n">
+          <cell :move="board[i][j]" @click="playerMove(i,j)"></cell>
+        </div>
+      </div>
+    <div v-if="gameOver">{{player1}} won</div>
   </div>
 </template>
-
 <script>
-export default {
-  name: "tickTackToe",
-  data() {
-    return {
-     board: [
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-       {move: ""},
-     ]
-    }
-  },
-  methods: {
-  },
-  computed: {
-    playerToken()  {
-      return "X";
-      //this.move = "X"; //when trying to move this computed to method, it breaks!!
-    }
+  export default {
+    data() { return {
+      player1: true,
+      gameOver: false,
+      board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ],
+      movesLeft: 9
+    } },
+    methods: {
+      playerMove(i,j) {
+        if(this.board[i][j] != "" || this.gameOver) return;
+        this.board[i][j] = this.Tocken();
+        this.$forceUpdate();
+        this.movesLeft = this.movesLeft - 1;
+        if(this.winner("X") || this.winner("O")){
+          this.gameOver = true;
+        }
+      },
+      GameOver(){
+        if(this.movesLeft == 0){
+          this.gameOver = true;
+        }
+      },
+      Tocken(){
+        if(this.player1){
+          this.player1 = !this.player1;
+          return "X";
+        }
+        else{
+          this.player1 = !this.player1;
+          return "O";
+        }
+      },
+      possibleMoves(){
+        return this.movesLeft;
+      },
+      winner(player){
+        //across
+        for(var i = 0; i < 3; i++){
+          if((this.board[0][i] == player) && (this.board[1][i] == player) && (this.board[2][i] == player)){
+            return true;
+          }
+        }
+        //down
+        for(i = 0; i < 3; i++){
+          if((this.board[i][0] == player) && (this.board[i][1] == player) && (this.board[i][2] == player)){
+            return true;
+          }
+        }
+
+        //vertical left to right
+          if((this.board[0][0] == player) && (this.board[1][1] == player) && (this.board[2][2] == player)){
+            return true;
+          }
+
+        //vertical right to left
+          if((this.board[2][0] == player) && (this.board[1][1] == player) && (this.board[0][2] == player)){
+            return true;
+          }
+
+          return false;
+      }
+    },
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.grid {
-  display: flex;
-  /* establish flex container */
-  flex-wrap: wrap;
-  /* enable flex items to wrap */
-  justify-content: space-around;
-  width: 150px;
-
-}
-
-.cell {
-  flex: 0 0 32%;
-  /* don't grow, don't shrink, width */
-  height: 50px;
-  margin-bottom: 5px;
-  background-color: #999;
-}
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    width: 204px;
+    height: 204px;
+  }
 </style>
